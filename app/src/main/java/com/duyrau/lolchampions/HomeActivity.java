79f86b7ab0693310212a93a9de3ce85a9ba87c6e
truongducduy <ducduytruong2012@gmail.com>
2015-11-28 21:39:29 +0700

    Create project structure

---
+++ app/src/main/java/com/duyrau/lolchampions/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.duyrau.lolchampions;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+}
