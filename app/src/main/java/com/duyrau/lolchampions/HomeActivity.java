65cc30d2c292d9e59807380f213028996dcee7d0
truongducduy <ducduytruong2012@gmail.com>
2015-12-04 00:15:24 +0700

    Add overflow menu on toolbar

--- app/src/main/java/com/duyrau/lolchampions/HomeActivity.java
+++ app/src/main/java/com/duyrau/lolchampions/HomeActivity.java
@@ -3,6 +3,8 @@ package com.duyrau.lolchampions;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -28,5 +30,27 @@ public class HomeActivity extends AppCompatActivity {
                 Toast.makeText(HomeActivity.this, "" + position, Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
