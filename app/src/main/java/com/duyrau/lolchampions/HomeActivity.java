38d251439479134e270068f728d008c0a12d6db3
truongducduy <ducduytruong2012@gmail.com>
2015-12-03 23:49:00 +0700

    Add gridview, toolbar

--- app/src/main/java/com/duyrau/lolchampions/HomeActivity.java
+++ app/src/main/java/com/duyrau/lolchampions/HomeActivity.java
@@ -2,8 +2,13 @@ package com.duyrau.lolchampions;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -11,6 +16,17 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        GridView gridView = (GridView)findViewById(R.id.gridview_champions);
+        gridView.setAdapter(new ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(HomeActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
