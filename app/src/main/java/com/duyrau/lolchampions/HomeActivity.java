87f1961e5cbd5877f0b3c06e19d76c2270888879
TRƯƠNG ĐỨC DUY <Duy.TruongDuc@vn.bosch.com>
2015-12-17 19:17:37 +0700

    Add search view

--- app/src/main/java/com/duyrau/lolchampions/HomeActivity.java
+++ app/src/main/java/com/duyrau/lolchampions/HomeActivity.java
@@ -1,7 +1,12 @@
 package com.duyrau.lolchampions;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -34,6 +39,16 @@ public class HomeActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+//        MenuItem searchItem = menu.findItem(R.id.action_search);
+//        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName() ));
+        searchView.setIconifiedByDefault(false);
+
+
         return true;
     }
 
